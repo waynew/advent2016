90c878d256774b524af60b5e483f14abd30c12c0
= <=>
2016-12-01 19:15:55 +0000

    Success on day 1.2

--- 01/02_bunny_hq.py
+++ 01/02_bunny_hq.py
@@ -27,21 +27,27 @@ class Direction(Enum):
         return new_directions[self]
 
 
-direction = Direction.north
-position = [0, 0]
-visited = set()
-
 steps = 'L1, R3, R1, L5, L2, L5, R4, L2, R2, R2, L2, R1, L5, R3, L4, L1, L2, R3, R5, L2, R5, L1, R2, L5, R4, R2, R2, L1, L1, R1, L3, L1, R1, L3, R5, R3, R3, L4, R4, L2, L4, R1, R1, L193, R2, L1, R54, R1, L1, R71, L4, R3, R191, R3, R2, L4, R3, R2, L2, L4, L5, R4, R1, L2, L2, L3, L2, L1, R4, R1, R5, R3, L5, R3, R4, L2, R3, L1, L3, L3, L5, L1, L3, L3, L1, R3, L3, L2, R1, L3, L1, R5, R4, R3, R2, R3, L1, L2, R4, L3, R1, L1, L1, R5, R2, R4, R5, L1, L1, R1, L2, L4, R3, L1, L3, R5, R4, R3, R3, L2, R2, L1, R4, R2, L3, L4, L2, R2, R2, L4, R3, R5, L2, R2, R4, R5, L2, L3, L2, R5, L4, L2, R3, L5, R2, L1, R1, R3, R3, L5, L2, L2, R5'.split(', ')
+#steps = 'R8, R4, R4, R8'.split(', ')
 
-for count, step in enumerate(steps):
-    turn = step[0]
-    distance = int(step[1:])
-    direction = direction.turn(turn)
-    position[0] += direction.value[0]*distance
-    position[1] += direction.value[1]*distance
-    pos = tuple(position)
-    if pos in visited:
-        print(pos, step, count)
-        print(sum(abs(x) for x in pos))
-        break
-    visited.add(pos)
+def find_crossover(steps):
+    direction = Direction.north
+    position = [0, 0]
+    visited = set()
+    visited.add((0,0))
+
+    for count, step in enumerate(steps):
+        turn = step[0]
+        distance = int(step[1:])
+        direction = direction.turn(turn)
+        for _ in range(distance):
+            position[0] += direction.value[0]
+            position[1] += direction.value[1]
+            pos = tuple(position)
+            if pos in visited:
+                print(pos, step, count)
+                return sum(abs(x) for x in pos)
+            visited.add(pos)
+    return None
+
+print(find_crossover(steps))
