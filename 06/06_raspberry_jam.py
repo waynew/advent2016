9e2e2e00b7c53f535d4a50a73291cb811ef46d1d
= <=>
2016-12-06 11:52:43 +0000

    Successful test day 6

---
+++ 06/06_raspberry_jam.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+counters = []
+with open('input.txt') as f:
+    counters = [Counter() for _ in next(f)]
+    f.seek(0)
+    for line in f:
+        for i, char in enumerate(line):
+            counters[i][char] += 1
+
+print(''.join(c.most_common(1)[0][0] for c in counters))
