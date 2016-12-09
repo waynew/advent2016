575c99c0c7b3a455fb06011b6f9af70e52c66de2
= <=>
2016-12-09 00:29:46 +0000

    Moar ascii-mation

--- 08/08_screen.py
+++ 08/08_screen.py
@@ -1,58 +1,50 @@
 from collections import deque
 
-pixels = [deque(None for _ in range(50))
-          for _ in range(6)
-          ]
-
-#pixels = [deque(None for _ in range(7))
-#          for _ in range(3)
-#          ]
-
-
-with open('input.txt') as f:
-    instructions = f.readlines()
-
-
-#instructions = [
-#    'rect 3x2',
-#    'rotate column x=1 by 1',
-#    'rotate row y=0 by 4',
-#    'rotate column x=1 by 1',
-#]
-
 def debug(pixels):
-    return '\n'.join(''.join('#' if cell else '.' for cell in row) for row in pixels)
+    return '\n'.join(''.join('\x1b[32m#\x1b[0m' if cell else '.' for cell in row) for row in pixels)
 
-for instruction in instructions:
-    op, _, rest = instruction.partition(' ')
-    if op == 'rect':
-        width, height = [int(val) for val in rest.split('x')]
-        for x in range(width):
-            for y in range(height):
-                pixels[y][x] = True
-    elif op == 'rotate':
-        if rest.startswith('column'):
-            col, amount = (int(val) for val in rest.split('=')[-1].split(' by '))
-            pixels = list(deque(z) for z in zip(*pixels[::-1]))
-            pixels[col].rotate(-amount)
-            #for _ in range(amount):
-            #    #new_vals = [pixels[(row+1)%len(pixels)][col] for row in range(len(pixels))]
-            #    new_vals = [pixels[row-1 if row > 0 else len(pixels)-1][col] for row in range(len(pixels))]
-            #    print(new_vals)
-            #    for row in range(len(pixels)):
-            #        pixels[row][col] = new_vals[row]
-            #        print(row, (row+1)%len(pixels), col)
-            pixels = list(reversed(list(deque(z) for z in zip(*pixels))))
+def display(instructions, width=50, height=6, sync_rate=0.1):
+    pixels = [deque(None for _ in range(width))
+              for _ in range(height)
+              ]
 
-        elif rest.startswith('row'):
-            row, amount = (int(val) for val in rest.split('=')[-1].split(' by '))
-            pixels[row].rotate(amount)
+    lit_count = 0
+    print(debug(pixels), end='')
+    for instruction in instructions:
+        op, _, rest = instruction.partition(' ')
+        if op == 'rect':
+            width, height = [int(val) for val in rest.split('x')]
+            for x in range(width):
+                for y in range(height):
+                    pixels[y][x] = True
+        elif op == 'rotate':
+            if rest.startswith('column'):
+                col, amount = (int(val) for val in rest.split('=')[-1].split(' by '))
+                pixels = list(deque(z) for z in zip(*pixels[::-1]))
+                pixels[col].rotate(-amount)
+                pixels = list(reversed(list(deque(z) for z in zip(*pixels))))
+
+            elif rest.startswith('row'):
+                row, amount = (int(val) for val in rest.split('=')[-1].split(' by '))
+                pixels[row].rotate(amount)
+            else:
+                assert False, 'Unknown direction '+rest
         else:
-            assert False, 'Unknown direction '+rest
-    else:
-        assert False, 'Unknown instruction '+op
-    print()
-    print(instruction)
-    print(debug(pixels))
+            assert False, 'Unknown instruction '+op
+        lit_count = sum(sum(1 for x in row if x) for row in pixels)
+        print('\x1b[5A', end='\r')
+        print(debug(pixels), end='\r')
+        print('\n{:*<50}'.format('*** {:>3} '.format(lit_count)), end='\x1b[1A')
+        import time; time.sleep(sync_rate)
 
-print('pixels lit:', sum(sum(1 for x in row if x) for row in pixels))
+    return lit_count
+
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        instructions = f.readlines()
+        for _ in range(8):
+            print('*'*50)
+        print('\x1b[7A', end='\r')
+        pixels_lit = display(instructions, sync_rate=0.1)
+        print()
