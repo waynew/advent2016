afa43c8b16c21c8e644779aff303bb545387a418
= <=>
2016-12-13 22:39:02 +0000

    maybe this works

--- 11/11_floors.py
+++ 11/11_floors.py
@@ -50,14 +50,17 @@ target = {
     'PR-C': 4,
 }
 
-def floor_to_text(items, floors=4, cur_floor=1):
+
+def floor_to_text(floors, cur_floor=1):
     text = []
-    for floor in range(1, floors+1):
-        if floor == cur_floor:
-            elevator = '\x1b[;41m{}\x1b[0m'.format(floor)
+    all_items = sum(floors, ())
+    for floor_num, floor in enumerate(floors, start=1):
+        if floor_num == cur_floor:
+            elevator = '\x1b[;41m{}\x1b[0m'.format(floor_num)
         else:
-            elevator = floor
-        text.insert(0, '{} {}'.format(elevator, ' '.join(colorize(item, items) if items[item] == floor else '  . ' for item in ['PL-C', 'PL-G', 'PR-C', 'PR-G', 'CO-C', 'CO-G', 'RU-C', 'RU-G', 'TH-C', 'TH-G'])))
+            elevator = floor_num
+        thing = ' '.join('{: ^{}}'.format((item if item in floor else '.'), len(all_items[0])) for item in all_items)
+        text.insert(0, '{} {}'.format(elevator, thing))
 
     return '\n'.join(text)
 
@@ -88,94 +91,206 @@ def can_go_up(item, items):
     return False
 
 
-def fried(items):
-    for floor in range(1, 5):
-        stuff_on_floor = [i for i in items if items[i] == floor]
-        for chip in (chip for chip in stuff_on_floor if chip.endswith('-C')):
-            if chip.replace('-C', '-G') not in stuff_on_floor and any(thing.endswith('-G') for thing in stuff_on_floor):
+def fried(floors):
+    for floor in floors:
+        for chip in (chip for chip in floor if chip.endswith('-C')):
+            if chip.replace('-C', '-G') not in floor and any(thing.endswith('-G') for thing in floor):
                 return True
 
 
-move_count = 0
-cur_floor = 1
-
-Hand = namedtuple('Hand', 'left,right')
-
-moves = [
-    (Hand('H-C', None), 1),
-    (Hand('H-C', 'H-G'), 1),
-    (Hand('H-C', None), -1),
-    (Hand('H-C', None), -1),
-    (Hand('H-C', 'L-C'), 1),
-    (Hand('H-C', 'L-C'), 1),
-    (Hand('H-C', 'L-C'), 1),
-    (Hand('H-C', None), -1),
-    (Hand('L-G', 'H-G'), 1),
-    (Hand('L-C', None), -1),
-    (Hand('L-C', 'H-C'), 1),
-]
-
-moves = [
-]
-
-els = ['PL', 'PR', 'CO', 'TH', 'RU']
+def to_dict(floors):
+    items = {}
+    for num, floor in enumerate(floors, 1):
+        for item in floor:
+            items[item] = num
+    return items
 
 
-#moves.append((Hand(els[0]+'-C', els[1]+'-C'), 1))
-#moves.append((Hand(els[0]+'-C', els[1]+'-C'), 1))
-#moves.append((Hand(els[0]+'-C', None), -1))
-#moves.append((Hand(els[0]+'-C', None), -1))
-for el in els[1:]:
-    moves.append((Hand(els[0]+'-C', el+'-C'), 1))
-    moves.append((Hand(els[0]+'-C', el+'-C'), 1))
-    moves.append((Hand(els[0]+'-C', el+'-C'), 1))
-    moves.append((Hand(els[0]+'-C', None), -1))
-    moves.append((Hand(els[0]+'-C', None), -1))
-    moves.append((Hand(els[0]+'-C', None), -1))
-moves.append((Hand(els[0]+'-C', els[0]+'-G'), 1))
-moves.append((Hand(els[0]+'-C', els[0]+'-G'), 1))
-moves.append((Hand(els[0]+'-G', None), -1))
-for el in els[1:]:
-    moves.append((Hand(els[0]+'-G', None), -1))
-    moves.append((Hand(els[0]+'-G', el+'-G'), 1))
-moves.append((Hand(els[0]+'-G', None), 1))
-moves.append((Hand(els[0]+'-C', els[0]+'-G'), -1))
-moves.append((Hand(els[0]+'-C', els[0]+'-G'), -1))
-moves.append((Hand(els[0]+'-G', None), 1))
-for el in els[1:]:
-    moves.append((Hand(els[0]+'-G', el+'-G'), 1))
-    moves.append((Hand(els[0]+'-G', None), -1))
-moves.append((Hand(els[0]+'-G', None), -1))
-moves.append((Hand(els[0]+'-C', els[0]+'-G'), 1))
-moves.append((Hand(els[0]+'-C', els[0]+'-G'), 1))
-for el in els[1:]:
-    moves.append((Hand(els[0]+'-C', None), 1))
-    moves.append((Hand(els[0]+'-C', el+'-C'), -1))
-moves.append((Hand(els[0]+'-C', els[0]+'-G'), 1))
+def to_tuple(items):
+    floors = []
+    for i in range(4):
+        floor_num = i+1
+        floors.append(tuple(sorted(item for item in items if items[item] == floor_num)))
+    return tuple(floors)
 
 
-print('Target:')
-print(floor_to_text(target))
-print('\n'+'*'*50)
-print('\n'*4)
-print('\x1b[4A', end='')
-print(floor_to_text(items, cur_floor=cur_floor))
-for move in moves:
-    time.sleep(0.1)
-    hand, count = move
-    cur_floor += count
-    if hand.left:
-        items[hand.left] += count
+def possible_moves(item, floors, floor_limit=4):
+    items = to_dict(floors)
+    elevator = items[item]
+    moves = []
 
-    if hand.right:
-        items[hand.right] += count
-    print('\r\x1b[4A', end='\r')
-    #print('*'*50)
-    print(floor_to_text(items, cur_floor=cur_floor))
-    if fried(items):
-        print('\nFried!')
-        break
-    if items == target:
-        print('\nWon in {} steps!'.format(len(moves-1)))
-    
-print('\nWon in {} steps!'.format(len(moves)-1))
+    other_items = [i for i in items if i != item and items[item] == items[i]]
+
+    next_floors = []
+    if elevator < floor_limit:
+        next_floors.append(elevator+1)
+    if elevator > 1:
+        next_floors.append(elevator-1)
+
+    for next_floor in next_floors:
+        for paired_item in other_items:
+            if items[paired_item] == items[item]:
+                items[paired_item] = next_floor
+                items[item] = next_floor
+                moves.append((next_floor, to_tuple(items)))
+                items[paired_item] = elevator
+                items[item] = elevator
+            items[item] = next_floor
+            moves.append((next_floor, to_tuple(items)))
+            items[item] = elevator
+
+    moves = [move for move in moves if not fried(move[1])]
+    return moves
+
+
+Move = namedtuple('Move', ('from_', 'to'))
+
+
+def find_shortest(floors, elevator=1, steps=0, seen=None):
+    start = (elevator, floors)
+    states = {
+        (elevator, floors): set(sum((possible_moves(item, floors, floor_limit=4) for item in floors[elevator-1]), []))
+    }
+    traversed_edges = set()
+    for dest in states[(elevator, floors)]:
+        states[dest] = None
+        traversed_edges.add(Move(from_=(elevator, floors), to=dest))
+        
+    while any(states[state] is None for state in states):
+        state = next(state for state in states if states[state] is None)
+        state_floor = state[0]-1
+        states[state] = states.get(state) or set()
+        for item in state[1][state_floor]:
+            for move in possible_moves(item, state[1], floor_limit=4):
+                states[state].add(move)
+                transition = Move(from_=state, to=move)
+                traversed_edges.add(transition)
+                states[move] = states.get(move, None)
+
+    distances = {state:float('inf') for state in states}
+    distances[start] = 0
+    unvisited = set(distances)
+
+    while unvisited:
+        current = min((distances[state], state) for state in unvisited)[1] 
+        unvisited.remove(current)
+        dist = distances[current]+1
+        for neighbor in states[current]:
+            distances[neighbor] = min(distances[neighbor], dist)
+
+    for thing in distances:
+        if not any(thing[1][:3]):
+            print(thing)
+    return distances
+
+
+if __name__ == '__main__':
+    target = (
+        (),
+        (),
+        (),
+        ('PL-G', 'TH-G', 'TH-C', 'PR-G', 'RU-G',
+         'RU-C', 'CO-G', 'CO-C', 'PL-C', 'PR-C',
+         ),
+    )
+    assert fried([['H-C', 'F-G'],])
+    assert not fried((['H-C', 'H-G'],))
+    assert fried((['H-C', 'H-G', 'F-C'],))
+    assert not fried((['H-C', 'H-G', 'F-C', 'F-G'],))
+    assert not fried((['H-G',  'F-G'], ['H-C', 'F-C']))
+
+
+    items = {
+        'H-C': 1,
+        'L-C': 1,
+        'H-G': 2,
+        'L-G': 3,
+    }
+
+    items = {
+        'PL-G': 1,
+        'TH-G': 1,
+        'TH-C': 1,
+        'PR-G': 1,
+        'RU-G': 1,
+        'RU-C': 1,
+        'CO-G': 1,
+        'CO-C': 1,
+        'PL-C': 2,
+        'PR-C': 2,
+    }
+    target = {
+        'PL-G': 4,
+        'TH-G': 4,
+        'TH-C': 4,
+        'PR-G': 4,
+        'RU-G': 4,
+        'RU-C': 4,
+        'CO-G': 4,
+        'CO-C': 4,
+        'PL-C': 4,
+        'PR-C': 4,
+    }
+
+    initial_state = (
+        ('H-C', 'L-C'),
+        ('H-G',),
+        ('L-G',),
+        (),
+    )
+    target = (
+        (), (), (),
+        tuple(sorted(('H-C', 'L-C', 'H-G', 'L-G')))
+    )
+
+    assert to_dict(initial_state) == to_dict(initial_state)
+    assert to_tuple(to_dict(initial_state)) == initial_state
+
+    valid_moves_hc = [
+        (2, (('L-C',),
+             ('H-C', 'H-G'),
+             ('L-G',),
+             (),
+             )
+         ),
+    ]
+    assert possible_moves('H-C', initial_state, floor_limit=4) == valid_moves_hc
+    assert possible_moves('L-C', initial_state, floor_limit=4) == []
+
+    assert possible_moves('H-G', (('L-C',), ('H-C', 'H-G'), ('L-G',), ())) == [(3, (('L-C',), (), ('H-C', 'H-G', 'L-G'), ())),
+                                                                               (3, (('L-C',), ('H-C',), ('H-G', 'L-G'), ())),
+                                                                               ]
+
+    initial_state = (
+        ('H-C', 'L-C'),
+        ('H-G',),
+        ('L-G',),
+        (),
+    )
+    target = (4, (
+        (), (), (),
+        tuple(sorted(('H-C', 'L-C', 'H-G', 'L-G')))
+    ))
+
+    print(find_shortest(initial_state, elevator=1)[target]-2)
+
+    initial_state = (
+        ('PL-G',
+         'TH-G',
+         'TH-C',
+         'PR-G',
+         'RU-G',
+         'RU-C',
+         'CO-G',
+         'CO-C',
+         ),
+        ('PL-C',
+         'PR-C',
+         ),
+         (),(),
+    )
+    target = (4, ((), (), (),
+                  ('PL-G', 'TH-G', 'TH-C', 'PR-G', 'RU-G',
+                   'RU-C', 'CO-G', 'CO-C', 'PL-C', 'PR-C',
+                   )))
+    print(find_shortest(initial_state, elevator=1)[target]-2)
